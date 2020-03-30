40f1d57c306e0340c1ff80cf409843832b8850cd
Manuel Mauky <manuel.mauky@saxsys.de>
2020-03-30 14:43:35 +0200

    refactor supporter page, rename saxsys to zeiss

---
+++ content/locations/zeiss.md
@@ -0,0 +1,7 @@
+---
+name: Carl Zeiss Digital Innovation
+street: Berliner Straße
+number: "63"
+zip: 02826
+city: Görlitz
+---
--- src/pages/unterstuetzer.js
+++ src/pages/unterstuetzer.js
@@ -28,14 +28,38 @@ const Address = ({ name, link, mapsLink, addressFields }) => (
   </div>
 )
 
-const UnterstuetzerPage = ({ data }) => {
-  const { saxsys_logo, mitp_logo, sands_logo, entwickler_tutorials_logo } = data
+const style = {
+  maxWidth: `${maxWidth}px`,
+  marginTop: "2em",
+  marginBottom: "3em",
+  borderBottom: "solid 1px #ddd",
+}
 
-  const style = {
-    maxWidth: `${maxWidth}px`,
-    marginBottom: "3em",
-    borderBottom: "solid 1px #ddd",
-  }
+
+
+const SupporterBoxMultiImage = ({links, children}) => (
+  <div style={style}>
+    <Row>
+      {links.map((linkTuple, i) => (
+        <Col key={i}>
+          <a aria-label="Link zum Sponsor" href={linkTuple.link}>
+            <Img style={linkTuple.logoStyle} fluid={linkTuple.logo.childImageSharp.fluid}/>
+          </a>
+        </Col>
+      ))}
+    </Row>
+    <p style={{marginTop: "1em"}}>
+      {children}
+    </p>
+  </div>
+)
+
+const SupporterBox = ({link, logo, children, logoStyle}) => <SupporterBoxMultiImage links={[{link, logo, logoStyle}]}>{children}</SupporterBoxMultiImage>
+
+
+
+const UnterstuetzerPage = ({ data }) => {
+  const { zeiss_logo, mitp_logo, sands_logo, entwickler_tutorials_logo } = data
 
   return (
     <Layout>
@@ -44,44 +68,24 @@ const UnterstuetzerPage = ({ data }) => {
       <h3>Sponsoren</h3>
       <p>Diese Firmen unterstützen uns!</p>
 
-      <div style={style}>
-        <a href="https://www.sogehtsoftware.de">
-          <Img fluid={saxsys_logo.childImageSharp.fluid} />
-        </a>
-        <p>
-          <strong>Saxonia Systems AG</strong> sponsert unser Catering und Getränke.
-        </p>
-      </div>
+      <SupporterBox
+        link="https://www.zeiss.com"
+        logo={zeiss_logo}
+        logoStyle={{maxHeight: "200px", maxWidth: "200px"}}
+      >
+          <strong>Carl Zeiss Digital Innovation</strong> sponsert unser Catering und Getränke.
+      </SupporterBox>
 
-      <div style={style}>
-        <a href="https://mitp.de/">
-          <Img fluid={mitp_logo.childImageSharp.fluid} />
-        </a>
-
-        <p>
+      <SupporterBox
+        link="https://mitp.de"
+        logo={mitp_logo}
+      >
           Der <strong>mitp Verlag</strong> unterstützt uns regelmäßig mit Sachpreisen zur Verlosung.
-        </p>
-      </div>
+      </SupporterBox>
 
-      <div style={style}>
-        <Row>
-          <Col>
-            <a href="https://entwickler.de/">
-              <Img fluid={entwickler_tutorials_logo.childImageSharp.fluid} />
-            </a>
-          </Col>
-
-          <Col>
-            <a href="https://sandsmedia.com/de">
-              <Img fluid={sands_logo.childImageSharp.fluid} />
-            </a>
-          </Col>
-        </Row>
-
-        <p>
+      <SupporterBoxMultiImage links={[{link: "https://entwickler.de/", logo: entwickler_tutorials_logo}, {link: "https://sandsmedia.com/de", logo: sands_logo}]}>
           <strong>Entwickler Press / S&S Media</strong> unterstützt uns regelmäßig mit Preisen zur Verlosung.
-        </p>
-      </div>
+      </SupporterBoxMultiImage>
 
       <p>
         Wir würden uns über weitere Unterstützer freuen! Sie können uns z. B. Preise für unsere Verlosungen unter den
@@ -109,8 +113,8 @@ const UnterstuetzerPage = ({ data }) => {
       />
 
       <Address
-        name="Saxonia Systems AG"
-        link="https://www.sogehtsoftware.de"
+        name="Carl Zeiss Digital Innovation"
+        link="https://www.zeiss.com"
         mapsLink="https://www.google.de/maps?q=G%C3%B6rlitz+Berliner+Stra%C3%9Fe+63&ie=UTF8&sll=51.139990000000004,14.964935399999993&sspn=0.25938504233517495,0.9385794045212804&t=m&dg=opt&hnear=Berliner+Stra%C3%9Fe+63,+02826+G%C3%B6rlitz&z=16"
         addressFields={["Berliner Straße 63", "02826 Görlitz"]}
       />
@@ -129,7 +133,7 @@ export default UnterstuetzerPage
 
 export const query = graphql`
   query {
-    saxsys_logo: file(relativePath: { eq: "saxsys_logo.jpg" }, sourceInstanceName: { eq: "images" }) {
+    zeiss_logo: file(relativePath: {eq: "zeiss-logo-rgb.png"}, sourceInstanceName: {eq: "images"}) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid
