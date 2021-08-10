c5fea74497255d11a7acbae96ec64b7da1ce218a
manuel-mauky <manuel.mauky@gmail.com>
2021-08-10 15:03:16 +0000

    Deploying to gh-pages from @ juggr/juggr.github.io@007b31a88640de7cb8b7f709e19224b884c016a9 🚀

---
+++ component---src-templates-static-page-js-cf5ca9135d92bb5b5aea.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{Pq2F:function(e,n,a){"use strict";a.r(n);var t=a("q1tI"),r=a.n(t),l=a("Bl7J");n.default=function(e){var n=e.data.markdownRemark;return r.a.createElement(l.a,null,r.a.createElement("div",{dangerouslySetInnerHTML:{__html:n.html}}))}}}]);
+//# sourceMappingURL=component---src-templates-static-page-js-cf5ca9135d92bb5b5aea.js.map
